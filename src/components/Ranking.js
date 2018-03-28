3f4e2ecadbb2c431462db3ae112e89f892f3a0af
Reginaldo Santos <rmsantos@br.ibm.com>
2018-03-28 14:42:21 -0300

    creating the Ranking component

---
+++ src/components/Ranking.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class Ranking extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            participantes: []
+        }
+        this.getParticipants = this.getParticipants.bind(this)
+
+        this.getParticipants()
+    }
+
+    getParticipants() {
+        const BASE_URL = 'https://wcbet-br.mybluemix.net'
+        const BASE_URL_DEV = 'http://localhost:6003'
+        const url = `${BASE_URL_DEV}/api/participante/lista`
+
+        axios.get(url)
+            .then(result => {
+                this.setState({
+                    participantes: result.data
+                })
+            })
+    }
+
+    render() {
+        console.log(this.state.participantes[0])
+        return (
+            <div>
+                <h2>Rankings</h2>
+                <ul>
+                    {
+                        this.state.participantes.map(user => {
+                            return <li key={user.IDPARTICIPANTES}>{user.NOME}</li>
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Ranking
